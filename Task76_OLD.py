c3ba07c18c5c2b07731e90bf75fdc7086130b041
Gabriel <gabrielbelo2007@gmail.com>
2024-05-12 19:14:30 -0300

    Atividade [76]

---
+++ Task76_OLD.py
@@ -0,0 +1,41 @@
+secreto = 'gostosa'
+digitadas = []
+chances = 5
+
+while True:
+    if chances <= 0:
+        print('Você perdeu!')
+        break
+
+    letra = input('Digite uma letra: ')
+
+    if len(letra) > 1:
+        print('Isso não vale, digite apenas uma letra.')
+        continue
+
+    digitadas.append(letra)
+
+    if letra in secreto:
+        print(f'Parabens "{letra}" existe na palavra secreta.')
+
+    else:
+        print(f'Que pena,"{letra}" NÃO EXISTE na palavra secreta.')
+        digitadas.pop()
+
+    descoberto = ''
+
+    for letra_secreta in secreto: # g
+        if letra_secreta in digitadas: # True
+            descoberto += letra_secreta # g
+        else:
+            descoberto += '*'
+
+    if descoberto == secreto:
+        print(f'Que legal, você ganhou! A palavra era {descoberto}.')
+        break
+
+    else:
+        print(f'A palavra secreta está assim: {descoberto}.')
+
+    if letra not in secreto:
+        chances -= 1
---
+++ Task76_palavra_secreta.py
@@ -0,0 +1,42 @@
+secreto = "teste"
+descoberto = []
+letras_usadas = ""
+tentativas = 3
+
+for caracter in secreto:
+    descoberto += "*"
+
+print(f"A palavra secreta tem: {len(secreto)} letras")
+
+while tentativas > 0:
+    
+    letra = input("Digite UMA letra: ")
+
+    indice = -1
+    if letra not in letras_usadas:
+        for caracter in secreto:  
+            indice += 1
+            if letra == caracter: 
+                descoberto[indice] = letra 
+        
+    else:
+        print("Você já usou essa letra")
+
+    letras_usadas += letra
+    
+    if letra not in secreto:
+        tentativas -= 1
+    else:
+        print(f"A letra: {letra}, existe na palavra secreta")
+
+    if descoberto == list(secreto):
+        descoberto_str = "".join(descoberto)
+        print(f"Parabéns você acertou a palavra era: {descoberto_str}")
+        break
+    
+    else:
+        print(f"Você tem {tentativas} tentativas, sua palavra: {descoberto}")
+    
+else:
+    secreto_str = "".join(secreto)
+    print("Suas tentativas acabaram a palavra era:", secreto_str)
