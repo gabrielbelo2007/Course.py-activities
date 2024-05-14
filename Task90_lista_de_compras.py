7055d4ff4fc5b5081c816146a072f470c9db0fa7
Gabriel <gabrielbelo2007@gmail.com>
2024-05-14 13:19:12 -0300

    Atividade [90] - Comentada

--- Task90_lista_de_compras.py
+++ Task90_lista_de_compras.py
@@ -1,3 +1,4 @@
+import os
 lista_compras = []
 
 print(f"Essa é sua lista inicial: ", lista_compras)
@@ -5,10 +6,12 @@ print(f"Essa é sua lista inicial: ", lista_compras)
 while True:
     escolha = input("Você quer [R]emover, [I]nserir ou [L]istar: ")
 
+    # Remove um item da lista
     if escolha[0].upper() == "R":
         print(lista_compras)
         remover = input("Remover da lista: ")
         
+        # Busca o item selecionado na lista
         for indice, valor in enumerate(lista_compras):
             if valor == remover:
                 lista_compras.pop(indice)
@@ -16,27 +19,34 @@ while True:
                 break
         else:
             print("Esse valor não existe na lista: ", lista_compras)
-                
+    
+    # Insere um item na lista
     elif escolha[0].upper() == "I":
         comprar = input("Inserir na lista: ")
         lista_compras.append(comprar)
         print(f"Essa é sua lista atual: ", lista_compras)
 
+    # Lista os itens na lista
     elif escolha[0].upper() == "L":
+        # Confere sem a lista pode ser listada
         if len(lista_compras) > 0:
             for indice, valor in enumerate(lista_compras):
                 print(indice, valor)
         else:
             print("A lista está vazia")
             continue
-            
+    
+    # Identifica se foi escolhido alguma das funcoes disponiveis     
     else:
         print("Escolha uma das funções disponíveis")
         continue
 
     confirmacao = input("Deseja continuar? [S]im ou [N]ao: ")
     
+    # Interrompe as alteracoes na lista e mostra a lista completa
     if confirmacao.upper() == "N":
-        print(f"Essa é sua lista final: ", lista_compras)
+        os.system("clear")
+        print(f"Essa é sua lista final: ")
+        for valor in lista_compras:
+            print(valor)
         break
-    
